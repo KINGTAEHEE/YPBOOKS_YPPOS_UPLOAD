2ba8e1c22a46164e286f02f5b12d14828ce39f8a
KINGTAEHEE <thk919@gmail.com>
2020-12-28 13:41:10 +0900

    프로젝트 업로드
    Visual Studio 2015 프로젝트 업로드

---
+++ YPPOS_UPLOAD/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace YPPOS_UPLOAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUploadFile = new System.Windows.Forms.Button();
+            this.listViewFileList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonAllCancel = new System.Windows.Forms.Button();
+            this.buttonAllSelect = new System.Windows.Forms.Button();
+            this.buttonUploadStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonUploadFile
+            // 
+            this.buttonUploadFile.Location = new System.Drawing.Point(12, 11);
+            this.buttonUploadFile.Name = "buttonUploadFile";
+            this.buttonUploadFile.Size = new System.Drawing.Size(130, 42);
+            this.buttonUploadFile.TabIndex = 1;
+            this.buttonUploadFile.Text = "업로드 파일 선택";
+            this.buttonUploadFile.UseVisualStyleBackColor = true;
+            this.buttonUploadFile.Click += new System.EventHandler(this.buttonUploadFile_Click);
+            // 
+            // listViewFileList
+            // 
+            this.listViewFileList.FormattingEnabled = true;
+            this.listViewFileList.ItemHeight = 12;
+            this.listViewFileList.Location = new System.Drawing.Point(148, 12);
+            this.listViewFileList.Name = "listViewFileList";
+            this.listViewFileList.Size = new System.Drawing.Size(424, 40);
+            this.listViewFileList.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonAllCancel);
+            this.groupBox1.Controls.Add(this.buttonAllSelect);
+            this.groupBox1.Location = new System.Drawing.Point(12, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(559, 415);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "대상 지점";
+            // 
+            // buttonAllCancel
+            // 
+            this.buttonAllCancel.Location = new System.Drawing.Point(473, 367);
+            this.buttonAllCancel.Name = "buttonAllCancel";
+            this.buttonAllCancel.Size = new System.Drawing.Size(80, 40);
+            this.buttonAllCancel.TabIndex = 1;
+            this.buttonAllCancel.Text = "전체해제";
+            this.buttonAllCancel.UseVisualStyleBackColor = true;
+            this.buttonAllCancel.Click += new System.EventHandler(this.buttonAllCancel_Click);
+            // 
+            // buttonAllSelect
+            // 
+            this.buttonAllSelect.Location = new System.Drawing.Point(387, 367);
+            this.buttonAllSelect.Name = "buttonAllSelect";
+            this.buttonAllSelect.Size = new System.Drawing.Size(80, 40);
+            this.buttonAllSelect.TabIndex = 0;
+            this.buttonAllSelect.Text = "전체선택";
+            this.buttonAllSelect.UseVisualStyleBackColor = true;
+            this.buttonAllSelect.Click += new System.EventHandler(this.buttonAllSelect_Click);
+            // 
+            // buttonUploadStart
+            // 
+            this.buttonUploadStart.Location = new System.Drawing.Point(235, 483);
+            this.buttonUploadStart.Name = "buttonUploadStart";
+            this.buttonUploadStart.Size = new System.Drawing.Size(130, 40);
+            this.buttonUploadStart.TabIndex = 4;
+            this.buttonUploadStart.Text = "업로드 시작";
+            this.buttonUploadStart.UseVisualStyleBackColor = true;
+            this.buttonUploadStart.Click += new System.EventHandler(this.buttonUploadStart_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 533);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(558, 16);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonUploadStart);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listViewFileList);
+            this.Controls.Add(this.buttonUploadFile);
+            this.Name = "Form1";
+            this.Text = "YPPOS_UPLOAD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUploadFile;
+        private System.Windows.Forms.ListBox listViewFileList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonAllCancel;
+        private System.Windows.Forms.Button buttonAllSelect;
+        private System.Windows.Forms.Button buttonUploadStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ YPPOS_UPLOAD/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace YPPOS_UPLOAD
+{
+    public partial class Form1 : Form
+    {
+        string[,] branch = new string[100, 2];
+        string[] fileSourceNames = new string[2];
+        int totalBranchCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {          
+            // 지점.txt 파일로부터 지점코드와 지점명을 불러온다
+            ReadBranchFile();
+            // 지점만큼 체크박스 버튼을 만든다
+            MakeCheckBoxButton();
+
+            progressBar1.Step = 100;
+        }
+
+        // 지점.txt 파일로부터 지점코드와 지점명을 불러온다
+        private void ReadBranchFile()
+        {
+            FileInfo file = new FileInfo("지점.txt");
+            if (file == null)
+            {
+                MessageBox.Show("지점.txt 파일이 존재하지 않습니다.\r확인해주세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            StreamReader sr = new StreamReader("지점.txt");
+            while (sr.EndOfStream == false)
+            {
+                string line = sr.ReadLine();
+                string[] split = line.Split('\t');
+                branch[totalBranchCount, 0] = split[0];
+                branch[totalBranchCount, 1] = split[1];
+                totalBranchCount++;
+            }
+            sr.Close();
+        }
+
+        private void MakeCheckBoxButton()
+        {
+            // 남은 열 갯수
+            int checkBoxCol = totalBranchCount % 4;
+            // 행 갯수
+            int checkBoxRow = totalBranchCount / 4;
+            // 카운터
+            int i = 0;
+
+            // 지점 수 4의 배수만큼 체크 박스를 만든다
+            for (int j = 0; j < checkBoxRow; j++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    CheckBox checkBox = new CheckBox();
+                    checkBox.Name = branch[i, 0];
+                    checkBox.Location = new Point(14 + (k * 135), 20 + (j * 22));
+                    checkBox.Size = new Size(130, 16);
+                    checkBox.Text = branch[i, 0] + " " + branch[i, 1];
+                    groupBox1.Controls.Add(checkBox);
+                    i++;
+                }
+            }
+
+            // 남은 지점 수(남은 열 갯수)만큼 체크 박스를 만든다
+            for (int j = 0; j < checkBoxCol; j++)
+            {
+                CheckBox checkBox = new CheckBox();
+                checkBox.Name = branch[i, 0];
+                checkBox.Location = new Point(14 + (j * 135), 20 + (checkBoxRow * 22));
+                checkBox.Size = new Size(130, 16);
+                checkBox.Text = branch[i, 0] + " " + branch[i, 1];
+                groupBox1.Controls.Add(checkBox);
+                i++;
+            }
+        }
+
+        private void buttonUploadFile_Click(object sender, EventArgs e)
+        {
+            ShowFileOpenDialog();
+        }
+
+        public void ShowFileOpenDialog()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // 담고있던 리스트뷰 및 파일명 정보 초기화
+                listViewFileList.Items.Clear();
+                fileSourceNames = new string[2];
+
+                foreach (string file in ofd.FileNames)
+                {
+                    try
+                    {
+                        listViewFileList.Items.Add(file);
+                        fileSourceNames = ofd.SafeFileNames;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("파일을 불러오는데 오류가 발생했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void buttonAllSelect_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in groupBox1.Controls)
+            {
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    if (cb.Checked == false)
+                    {
+                        cb.Checked = true;
+                    }
+                }
+            }
+        }
+
+        private void buttonAllCancel_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in groupBox1.Controls)
+            {
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    if (cb.Checked == true)
+                    {
+                        cb.Checked = false;
+                    }
+                }
+            }
+        }
+
+        private void buttonUploadStart_Click(object sender, EventArgs e)
+        {
+            string fileTargetPath1 = @"D:\pos\POSDATA\PGM\";
+            // 체크된 체크 박스 갯수 카운터
+            int i = 0;
+
+            if (MessageBox.Show("파일 업로드를 진행하시겠습니까?", "확인", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                // 선택된 파일이 없으면 메시지박스 띄운다
+                if (listViewFileList.Items.Count == 0)
+                {
+                    MessageBox.Show("선택된 파일이 없습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                // 선택된 파일이 1개일 때
+                else if (listViewFileList.Items.Count == 1)
+                {
+                    try
+                    {
+                        foreach (Control c in groupBox1.Controls)
+                        {
+                            if (c is CheckBox)
+                            {
+                                CheckBox cb = (CheckBox)c;
+                                if (cb.Checked == true)
+                                {
+                                    i++;
+                                }
+                            }
+                        }
+
+                        foreach (Control c in groupBox1.Controls)
+                        {
+                            if (c is CheckBox)
+                            {
+                                CheckBox cb = (CheckBox)c;
+                                if (cb.Checked == true)
+                                {
+                                    string fileTargetPath2 = cb.Name;
+                                    System.IO.Directory.CreateDirectory(fileTargetPath1 + fileTargetPath2);
+                                    System.IO.File.Copy(listViewFileList.Items[0].ToString(), fileTargetPath1 + fileTargetPath2 + "\\" + fileSourceNames[0], true);
+                                }
+                            }
+                        }
+                        // 선택된 파일은 있지만 선택된 지점이 없으면 메시지박스 띄운다
+                        if (i == 0)
+                        {
+                            MessageBox.Show("선택된 지점이 없습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("파일 업로드에 실패했습니다.\r파일이 올바른지 확인해주세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    progressBar1.PerformStep();
+                    i = 0;
+                    MessageBox.Show("파일 업로드를 완료했습니다.", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                // 선택된 파일이 2개일 때
+                else if (listViewFileList.Items.Count == 2)
+                {
+                    try
+                    {
+                        foreach (Control c in groupBox1.Controls)
+                        {
+                            if (c is CheckBox)
+                            {
+                                CheckBox cb = (CheckBox)c;
+                                if (cb.Checked == true)
+                                {
+                                    i++;
+                                }
+                            }
+                        }
+
+                        foreach (Control c in groupBox1.Controls)
+                        {
+                            if (c is CheckBox)
+                            {
+                                CheckBox cb = (CheckBox)c;
+                                if (cb.Checked == true)
+                                {
+                                    string fileTargetPath2 = cb.Name;
+                                    System.IO.Directory.CreateDirectory(fileTargetPath1 + fileTargetPath2);
+                                    System.IO.File.Copy(listViewFileList.Items[0].ToString(), fileTargetPath1 + fileTargetPath2 + "\\" + fileSourceNames[0], true);
+                                    System.IO.File.Copy(listViewFileList.Items[1].ToString(), fileTargetPath1 + fileTargetPath2 + "\\" + fileSourceNames[1], true);
+                                    progressBar1.PerformStep();
+                                }
+                            }
+                        }
+                        // 선택된 파일은 있지만 선택된 지점이 없으면 메시지박스 띄운다
+                        if (i == 0)
+                        {
+                            MessageBox.Show("선택된 지점이 없습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("파일 업로드에 실패했습니다.\r파일이 올바른지 확인해주세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    progressBar1.PerformStep();
+                    i = 0;
+                    MessageBox.Show("파일 업로드를 완료했습니다.", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("파일 갯수가 잘못됐습니다. 확인해주세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+        }
+    }
+}
